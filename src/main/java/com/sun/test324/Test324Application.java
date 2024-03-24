4ffb009073842c153a6b4eb97d40e9d5762958b9
szh <2667429120@qq.com>
2024-03-24 22:18:45 +0800

    测试

---
+++ src/main/java/com/sun/test324/Test324Application.java
@@ -0,0 +1,13 @@
+package com.sun.test324;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test324Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test324Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sun/test324/Test324ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sun.test324;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test324ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
